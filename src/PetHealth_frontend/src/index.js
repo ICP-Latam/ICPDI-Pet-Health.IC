6bd2a4ebffef1c8ada0754169971e925d691c87a
joycelozano <joyce.lozag@gmail.com>
2023-05-12 10:42:06 -0600

    Initial commit.

---
+++ src/PetHealth_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { PetHealth_backend } from "../../declarations/PetHealth_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await PetHealth_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
